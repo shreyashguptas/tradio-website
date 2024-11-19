f4172b21859b7ffb40895f7df03d1c7ba8ade581
Shreyash Gupta <shreyash.gupta@shreyashg.com>
2024-11-19 08:12:32 -0600

    Adding Speed Insights from Vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Montserrat } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next" // Adding the package for Vercel Speed Insights
 
 const montserrat = Montserrat({
   subsets: ["latin"],
